4a395b06dedd1c57c312327e349f0e38febf1e6c
Satoe Sakonjo <sakonjo310@gmail.com>
2023-02-22 14:37:34 -0800

    Fix bug and improve style

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react'
 import TodoItem from './TodoItem';
 import axios from 'axios'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 
 
 const Form = () => {
   const randomNumber = () => Math.floor(Math.random() * 150) + 1;
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
-  const [finished, setFinished] = useState(false)
+  const [finished, setFinished] = useState(false);
+  const [error, setError] = useState(false)
 
   const handleChange = (e) => {
     setTodo(e.target.value)
@@ -16,42 +20,43 @@ const Form = () => {
   const handleClick = (e) => {
     e.preventDefault();
     if (todo.length > 0) {
-      setTodos([...todos, {id: Math.random(), todo: todo, finished: finished}]);
+      setTodos([...todos, {id: Math.random(), todo: todo, isFinished: finished}]);
       setFinished(false);
       setTodo("")
+      setError(false)
     } else {
-      alert("Task name cannot be empty")
+      setError(true)
     }
   }
   // console.log(todos);
 
-  const handleChangeCheckbox = () => {
-    setFinished(!finished);
-  }
+  // const handleChangeCheckbox = () => {
+  //   setFinished(!finished);
+  // }
 
   const handleDelete = (id) => {
     const afterDelete = todos.filter(todo => todo.id !== id);
     console.log("AFTER:", afterDelete)
-    setTodos([...afterDelete])
+    setTodos(afterDelete)
   }
   console.log("WHAT ABOUT NOW:", todos)
 
-  const handleRandomClick = () => {
-    const url = `https://dummyjson.com/todos/${randomNumber()}`;
-    axios.get(url)
-      .then(response => {
-        console.log(response.data);
-        const data = response.data;
-        setTodos(prev => [...prev, {id: data.id, todo: data.todo, finished: false}])
-      })
-  }
+  // const handleRandomClick = () => {
+  //   const url = `https://dummyjson.com/todos/${randomNumber()}`;
+  //   axios.get(url)
+  //     .then(response => {
+  //       console.log(response.data);
+  //       const data = response.data;
+  //       setTodos(prev => [...prev, {id: data.id, todo: data.todo, isFinished: false}])
+  //     })
+  // }
 
-  const todoItems = todos.map((todo, i) => {
+  const todoItems = todos.map((todo) => {
     return (
           <TodoItem 
-            key={i} 
+            key={todo.id} 
             task={todo.todo} 
-            finished={finished} 
+            isFinished={todo.isFinished} 
             id={todo.id} 
             handleDelete={handleDelete}
           />
@@ -61,19 +66,26 @@ const Form = () => {
   return (
     <div>
       <form>
-        <label>
-          What task do you want to add?
           <br />
-          <input type="text" name="text" value={todo} onChange={handleChange} />
-        </label>
+          {/* <input type="text" name="text" value={todo} onChange={handleChange} /> */}
+          <TextField
+            helperText={error ? "Task cannot be empty." : ""}
+            error={error ? true : false}
+            id={error ? "outlined-error" : "outlined-basic"}
+            label="What task do you want to add?"
+            value={todo}
+            onChange={handleChange}
+            sx={{width:"500px", mb:3}}
+          />
         <br />
-        <label htmlFor='finished'>Finished?</label>
+        {/* <label htmlFor='finished'>Finished?</label>
         <input id="finished" type="checkbox" checked={finished} name="checkbox" onChange={handleChangeCheckbox}/>
-        <br />
-        <input type="submit" name="submit" onClick={handleClick}/>
+        <br /> */}
+        {/* <input type="submit" name="submit" onClick={handleClick}/> */}
+        <Button variant="outlined" type="submit" onClick={handleClick}>Add Task</Button>
         <br />
       </form>
-      <button onClick={handleRandomClick}>Get Random</button>
+      {/* <button onClick={handleRandomClick}>Get Random</button> */}
       <div className='todoItem'>{todoItems}</div>
     </div>
   )
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,22 +1,25 @@
 import React, { useState } from 'react'
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Checkbox from '@mui/material/Checkbox';
 
 const TodoItem = (props) => {
-  const [finished, setFinished] = useState(props.finished);
+  const [finished, setFinished] = useState(props.isFinished);
   console.log("FINAL:", finished)
   const onChangeHandler = () => {
     setFinished(!finished)
   }
 
-  const HandleClick = (id) => {
-    props.handleDelete(id)
-  }
 
   return (
     <div>
       <span className={finished ? "finished" : ""}>{props.task}</span>
-      <input type="checkbox" checked={finished} onChange={onChangeHandler}/>
+      <Checkbox checked={finished} onChange={onChangeHandler}/>
       {/* {finished && <span>Finished</span>} */}
-      <button onClick={() => HandleClick(props.id)}>Delete</button>
+      <IconButton aria-label="delete" onClick={() => props.handleDelete(props.id)}>
+        <DeleteIcon />
+      </IconButton>
+      {/* <button onClick={() => props.handleDelete(props.id)}>Delete</button> */}
     </div>
   )
 }
